69c4e82a157f490d6455653d892ae2a08c0e6186
Ballabh Heera <44017701+heerajoshi@users.noreply.github.com>
2018-10-24 18:28:40 +0530

    Separate run test file or lib test

--- run_all_test.sh
+++
@@ -1,7 +0,0 @@
-set -e
-
-./run_test.sh rectanglePattern.js ./test/create_rectangle_input ./test/create_rectangle_output
-
-./run_test.sh trianglePattern.js ./test/create_triangle_input ./test/create_triangle_output
-
-./run_test.sh diamondPattern.js ./test/create_diamond_input ./test/create_diamond_output
